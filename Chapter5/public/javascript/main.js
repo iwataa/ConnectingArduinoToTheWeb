bc8150fdbe00fd86bb4f27a58bc98215b94e0a80
iwataa <iwataa@users.noreply.github.com>
2018-12-19 13:50:14 +0900

    Chapter5 pp.141-148
    Add serial port connection

--- Chapter5/public/javascript/main.js
+++ Chapter5/public/javascript/main.js
@@ -1,11 +1,16 @@
-var redBlock = document.getElementById("red-block");
-var greenBlock = document.getElementById("green-block");
+(function() {
+	
+	var socket = io();
+	var redBlock = document.getElementById("red-block");
+	var greenBlock = document.getElementById("green-block");
 
-redBlock.addEventListener("click", () => {
-	redBlock.classList.toggle("red-block-on");
-});
+	redBlock.addEventListener("click", () => {
+		var redClick = redBlock.classList.toggle("red-block-on");
+		socket.emit('red', redClick + "_red");
+	});
 
-greenBlock.addEventListener("click", () => {
-	greenBlock.classList.toggle("green-block-on");
-});
-
+	greenBlock.addEventListener("click", () => {
+		var greenClick = greenBlock.classList.toggle("green-block-on");
+		socket.emit('green', greenClick + "_green");
+	});
+})();
